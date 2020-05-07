470d8dfb331cd5b49516b53a1a6bf59162576acf
Daniel Fryar <daniel.fryar@gmail.com>
2020-05-07 12:01:01 -0500

    Adding AdImages, Finished with Controller Layer, Views to create, still need to include the images on the view add page

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -5,6 +5,7 @@ import com.codeup.adlister.Config;
 public class DaoFactory {
     private static Ads adsDao;
     private static Users usersDao;
+    private static AdImages adImagesDao;
     private static Config config = new Config();
 
     public static Ads getAdsDao() {
@@ -19,5 +20,12 @@ public class DaoFactory {
             usersDao = new MySQLUsersDao(config);
         }
         return usersDao;
+    }
+
+    public static AdImages getAdImagesDao() {
+        if (adImagesDao == null) {
+            adImagesDao = new MySQLAdImagesDao(config);
+        }
+        return adImagesDao;
     }
 }
